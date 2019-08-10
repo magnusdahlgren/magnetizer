1cbb230e7e310e26e5af2acb94ceb5a32727a49b
Magnus Dahlgren <hello@magnusdahlgren.com>
2019-08-10 06:00:42 +0100

    Adding -config parameter to command line - resolves #51

--- bin/magnetizer.py
+++ bin/magnetizer.py
@@ -1,8 +1,16 @@
 from webpage import *
+from sys import argv
 
 def main():
 
-    website = Website('../config/magnetizer.cfg')
+    if len(argv) == 3 and argv[1] == '-config':
+        config_filename = argv[2]
+    else:
+        config_filename = '../config/magnetizer.cfg'
+
+    print('Using config ' + config_filename + '...')
+
+    website = Website(config_filename)
     website.wipe()
 
     Webpage.write_webpages_from_directory(website, website.config.value('source_path'))
