aa5aa877358a677bbcc43e238de743cfc350272d
Magnus Dahlgren <me@magnusd.cc>
2019-11-12 11:12:37 +0000

    Adding cache buster for CSS - resolves #86

--- bin/website.py
+++ bin/website.py
@@ -1,6 +1,7 @@
 from os import listdir, path, remove, mkdir, rename
 from config import *
 import shutil
+import hashlib
 from mutil import *
 
 class Website:
@@ -31,8 +32,10 @@ class Website:
         self.index_header_html = Website.read_file(self.config.value('template_path'), self.config.value('index_header_template_filename'))
         self.index_footer_html = Website.read_file(self.config.value('template_path'), self.config.value('index_footer_template_filename'))
         self.list_page_header_html = Website.read_file(self.config.value('template_path'), self.config.value('list_page_header_template_filename'))
-        self.css_filename = self.config.value('website_css_filename')
 
+        css_contents = Website.read_file(self.config.value('resources_path'), self.config.value('website_css_filename'))
+        css_hash = hashlib.md5(bytes(css_contents, encoding='utf-8')).hexdigest()
+        self.css_filename = self.config.value('website_css_filename') + '?' + css_hash
 
     def copy_resources(self):
 
--- tests/test_website.py
+++ tests/test_website.py
@@ -7,4 +7,4 @@ test_website.refresh()
 
 def test_website_css_filename():
 
-    assert test_website.css_filename == "test-stylesheet.css"
+    assert test_website.css_filename == "test-stylesheet.css?c2459f7c370ffd41171a4265a4132352"
