b95f5149a304775a22f4db92f7494cca874181c0
Magnus Dahlgren <me@magnusd.cc>
2019-12-22 08:29:17 +0100

    Adding sitemap to Pylint

--- magnetizer/sitemap.py
+++ magnetizer/sitemap.py
@@ -1,6 +1,12 @@
-from mutil import *
+""" A module to generate a sitemap, i.e. a file with a list of URLs for search engines
+to index.
+"""
+
+from mutil import colours
 
 class Sitemap:
+    """ A class to populate a sitemap and then write it to file
+    """
 
     def __init__(self, base_url):
 
@@ -9,24 +15,28 @@ class Sitemap:
 
 
     def append(self, page):
+        """ Add page to sitemap (or '/' if page is index.html)
+        """
 
         if page == 'index.html':
-            p = ''
-        else:
-            p = page 
-        self.pages.append(self.base_url + '/' + p)
+            page = ''
+        self.pages.append(self.base_url + '/' + page)
 
 
     def write(self, output_path):
+        """ Write sitemap to sitemap.txt in output_path
+        """
 
         print('Generating sitemap --> %s' % output_path)
-        n = 0
+        counter = 0
         with open(output_path + 'sitemap.txt', 'w') as myfile:
-            for page in self.pages:                    
+            for page in self.pages:
                 myfile.write(page + '\n')
-                n += 1
-        print (colours.OK + ' --> ' + colours.END + 'Generated sitemap with %s items' % n)
+                counter += 1
+        print(colours.OK + ' --> ' + colours.END + 'Generated sitemap with %s items' % counter)
 
 
     def clear(self):
+        """ Remove all pages from sitemap
+        """
         self.pages = []
