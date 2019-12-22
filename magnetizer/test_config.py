1552f495600d4658e9261998cb1ccddc37f7be12
Magnus Dahlgren <me@magnusd.cc>
2019-12-22 18:33:30 +0100

    Making test_config.py Pylint compliant

--- magnetizer/test_config.py
+++ magnetizer/test_config.py
@@ -1,36 +1,27 @@
-import pytest
-from magnetizer import *
+""" Test to ensure Config works as expected
+"""
+
 from config import Config
 
-config = Config('tests/config/test.cfg')
+CONFIG = Config('tests/config/test.cfg')
 
 
-def test_basic_config():
+def test_config():
+    """ Test using test.cfg to ensure key-value pairs are set correctly
+    """
 
-    assert config.value('variable_number_one') == 'variable number one'
-    assert config.value('variable_number_two') == '2'
-    assert config.value('variable_number_three') == 'number = 3'
+    assert CONFIG.value('variable_number_one') == 'variable number one'
+    assert CONFIG.value('variable_number_two') == '2'
+    assert CONFIG.value('variable_number_three') == 'number = 3'
 
-
-def test_array_in_config():
-
-    expected = ['value 1','value 2', 'Value 3']
-
-    assert len(config.value('variable_with_array')) == len(expected)
-
-    for i in range(len(config.value('variable_with_array'))):
-        assert  config.value('variable_with_array')[i] == expected[i]
-
-
-@pytest.mark.skip(reason="Not sure how to test exceptions")
-def test_exception_when_key_undefined():
-
-    assert False
-
-    # todo: check for key error
+    assert len(CONFIG.value('variable_with_array')) == 3
+    for i in range(len(CONFIG.value('variable_with_array'))):
+        assert  CONFIG.value('variable_with_array')[i] == ['value 1', 'value 2', 'Value 3'][i]
 
 
 def test_set_config():
+    """ Test to ensure it is possible to set key-value pair
+    """
 
-    config.set('my_key', 'my_value')
-    assert config.value('my_key') == 'my_value'
+    CONFIG.set('my_key', 'my_value')
+    assert CONFIG.value('my_key') == 'my_value'
