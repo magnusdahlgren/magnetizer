f2ad517ed6eda69b8ca763ff63550ab98f576c2b
Magnus Dahlgren <me@magnusd.cc>
2019-12-22 17:16:58 +0100

    Updating template.py to be (almost) pylint compliant

--- magnetizer/template.py
+++ magnetizer/template.py
@@ -1,4 +1,14 @@
+""" A module to provide simple html templates
+
+"""
 class Template:
+    """ Template
+
+    Parameters:
+    content_tag - the tag used in the template to show where the content should go
+    filename - the name of the file to read the template from
+    """
+    # pylint: disable=too-few-public-methods
 
     def __init__(self, content_tag, filename):
 
@@ -11,5 +21,13 @@ class Template:
             self.template = self.content_tag
 
     def render(self, html):
+        """ Renders a page or partial by inserting the given html into the template
+
+        Parameters:
+        html - The html block to insert into the template
+
+        Returns:
+        The html of the template with the given html content inserted at the appropriate place
+        """
 
         return self.template.replace(self.content_tag, html, 1)
